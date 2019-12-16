fb29e655c3b09231da0a180441f4e4064242a84b
Labrajor <jordyvdwijngaard@gmail.com>
2019-12-16 23:16:47 +0100

    JSON integration

--- HomeImprovement/Raspberry C++/main.cpp
+++ HomeImprovement/Raspberry C++/main.cpp
@@ -6,10 +6,42 @@
  */
 
 #include "Control.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/prettywriter.h"
+#include "rapidjson/document.h"
+
+using namespace rapidjson;
 
 int main()
 {
-    Control c1; // create control class
+    FILE* rp = fopen("testJSON.json", "r"); // non-Windows use "r"
+ 
+    char readBuffer[65536];
+
+    FileReadStream is(rp, readBuffer, sizeof(readBuffer));
+
+    Document d;
+    d.ParseStream(is);
+
+    cout << readBuffer << endl;
+
+    fclose(rp);
+
+    FILE* wp = fopen("testJSON.json", "w"); // non-Windows use "w"
+ 
+    char writeBuffer[65536];
+    FileWriteStream os(wp, writeBuffer, sizeof(writeBuffer));
+
+    Value& s = d["Age"];
+    s.SetInt(s.GetInt() + 1);
+ 
+    PrettyWriter<FileWriteStream> writer(os);
+    d.Accept(writer);
+ 
+    fclose(wp);
+
+    Control c1;
 
     while(1)
     {
