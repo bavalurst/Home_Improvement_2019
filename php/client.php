1df208750e7d75490ced4a5f8881e379ed91af8e
willemvandergaag <37209583+willemvandergaag@users.noreply.github.com>
2019-12-19 17:45:03 +0100

    comments

--- php/client.php
+++ php/client.php
@@ -123,16 +123,16 @@
 		$sock = socket_create(AF_INET, SOCK_STREAM, 0);
 		socket_connect($sock, $host, $port);
 
-		socket_write($sock, $msg, strlen($msg));           // schrijf bericht gesplitst door komma
+		socket_write($sock, $msg, strlen($msg));           // stuur het bericht
 
 		$reply = socket_read($sock, 1924);
 		$reply = trim($reply);
 
-		$reply = "Server says:\t".$reply;
+		$reply = "Server says:\t".$reply;				// zichtbaar in terminal
     }
     
-    if (isset($_POST['toggle1'])){
-        Write("0,5");
+    if (isset($_POST['toggle1'])){						// testknop
+        Write("0,5");	
     }
 
    ?>
