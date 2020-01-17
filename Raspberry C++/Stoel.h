bf34762ce24ab9dd2777520bbb8cd5046fef4493
Arendonk <sjarendonk@gmail.com>
2020-01-17 15:41:54 +0100

    Stoel is af

--- Raspberry C++/Stoel.h
+++ Raspberry C++/Stoel.h
@@ -2,14 +2,32 @@
 #define STOEL_H
 
 #include "Device.h"
+#include <ctime>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Stoel : public Device
 {
 private:
     /* data */
 	Actuator* a1;
+	Actuator* a2;
 	Sensor* s1;
+	Sensor* s2;
 
+	time_t timeStart;
+	time_t alarm = 0;
+	time_t cooldown = 0;
+	int seizureAlert = 0;
+	int previousPressure = 0;
+	int currentPressure = 0;
+
+	int oneSecond = 0;
+	int seizureTime = 0;
+	int seizureLevel = -1;
+	int lastAct = 0;
 public:
     Stoel(char*);
     virtual ~Stoel();
