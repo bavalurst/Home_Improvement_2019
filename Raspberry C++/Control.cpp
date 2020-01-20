1ec040530de976aec13092784f5d5b04373eb781
bavalurst <felletje95@gmail.com>
2020-01-20 17:33:32 +0100

    Comments

--- Raspberry C++/Control.cpp
+++ Raspberry C++/Control.cpp
@@ -34,7 +34,7 @@ Control::~Control()
 
 void Control::addDevice(string key, Device* d1)
 {
-    devices.insert(make_pair(key, d1));
+    devices.insert(make_pair(key, d1)); //Adds device to map
 }
 
 
@@ -43,48 +43,49 @@ void Control::compareDatabaseToDevice()
     // compare the values from the map with databases and the map from devices with each other, and act accordingly
     for (map<string, Device*>::iterator dev = devices.begin(); dev != devices.end(); ++dev)
     {
-        map<string, Actuator*> a1 = dev->second->getActuators();
-        map<string, Sensor*> sensors;
+        map<string, Actuator*> a1 = dev->second->getActuators(); //gets a map of actuators from device located at iterator from device map
+        map<string, Sensor*> sensors;                            //initialize sensormap and iterator
         map<string, Sensor*>::iterator it;
         sensors = dev->second->getSensors();
 
-        for(map<string, Actuator*>::iterator act = a1.begin(); act != a1.end(); ++act)
+        for(map<string, Actuator*>::iterator act = a1.begin(); act != a1.end(); ++act) //for each actuator in device..
         {
-            string key = act->first;
+            string key = act->first; //get the key of the actuator, used to get the right values from actuator.json
 
-            if(!(act->second->getValue() == dat->readActuatorData(key)))
+            if(!(act->second->getValue() == dat->readActuatorData(key))) //if the value in actuator.json is not equal to the value inside actuator object.
+		    							//the actuator value will be equal to the actuator.json, and the value will be sent to the device. 
             {
             	//cout << "Sending: " << dat->readActuatorData(key) << "To Key: " << key << endl;
             	act->second->setValue(dat->readActuatorData(key));
                 dev->second->sendMessage(act->second->getKey() + dat->readActuatorData(key));
             }
         }
-        medicine();
+        medicine(); //initialize medicine check
         usleep(100000); // wait 100ms to prevent socket failure
 
-        string buffer = dev->second->receiveMessage();
-        vector<string> result;
-        result = parseMessage(buffer);
+        string buffer = dev->second->receiveMessage(); //new string is equal to receive message from device in loop
+        vector<string> result; //initialize vector
+        result = parseMessage(buffer); //convert the received message into a vector<string> with parsed key values (see parseMessage Comments for more info)
 
-        for(int i=0; i < result.size(); i+=2)
+        for(int i=0; i < result.size(); i+=2) //for each entry in the vector
         	{
-        		it = sensors.find(result[i]);
+        		it = sensors.find(result[i]); //find the sensorlocation in map with the key found in the result vector.
 
-        		if(it != sensors.end()) {
+        		if(it != sensors.end()) { //for that sensor, set the value of the sensor equal to the value recieved earlier, and store it in the sensor.json file
         			it->second->setValue(result[i + 1]);
         			dat->updateSensorData(result[i], result[i + 1]);
-        			cout << "Updating with key: " << result[i] << " Value: " << result[i+1] << endl;
+        			cout << "Updating with key: " << result[i] << " Value: " << result[i+1] << endl; 
         		}
             }
 
-        string SaveThis = dev->second->logic(devices);
+        string SaveThis = dev->second->logic(devices); //execute logic from device, returns an empty string if nothing should be done. Returns a key/value string (to be parsed) if actuator values should change.
         if(SaveThis != "") {
         	vector<string> stringresult;
         	stringresult = parseMessage(SaveThis);
         	for(int i=0; i < stringresult.size(); i+=2) {
         		cout << "Writing to: " << stringresult[i];
         		cout << " with value: " << stringresult[i+1] << endl;
-        		dat->writeActuatorData(stringresult[i], stringresult[i+1]);
+        		dat->writeActuatorData(stringresult[i], stringresult[i+1]); //To change actuator values, they will be written in the database. The next loop will catch it, and send the proper message.
         	}
 
         }
@@ -93,34 +94,34 @@ void Control::compareDatabaseToDevice()
 }
 
 
-vector<string> Control::parseMessage(string message)
+vector<string> Control::parseMessage(string message) //parses messages recieved by wemos or commands by this code and returns them in a vector<string>
 {
-  vector<string> stringlist;
+  vector<string> stringlist; //initialize empty vector
   string result;
   string result2;
   string temp;
 
   int i;
-  for (i=0; i != message.size(); i++)   {
+  for (i=0; i != message.size(); i++)   { //loop through each character in a string
 
-    if (!(message[i] == ';' || message[i] == ':'))
-    temp += message[i];
+    if (!(message[i] == ';' || message[i] == ':')) //for each character which is not a ':' or a ';'
+    temp += message[i]; //add this character to a temporary string
 
-    else {
-        stringlist.push_back(temp);
-        temp = "";
+    else { //if the loop hit a ':' or ';'
+        stringlist.push_back(temp); //add the temporary string to the vector
+        temp = ""; //and empty the temporary, for repetitions
     }
 
     }
 
-    if (result[5] == '\0') { cout << "BINGO: " << result[5] << endl;}
-	return stringlist;
+    //if (result[5] == '\0') { cout << "BINGO: " << result[5] << endl;}
+    return stringlist;
 
 }
 
 void Control::medicine() {
 	//cout << Time << endl;
-	if ((time(nullptr) - Time) >= 60) {
+	if ((time(nullptr) - Time) >= 60) { 
 		//cout << endl << endl << endl << "Medicijnen innemen!" << endl << endl << endl;
 		dat->writeActuatorData("35", "1");
 		Time = time(nullptr);
