df4517d5ce8acca4892f0110d994e86f3d4fd870
Sander Bosman <15093638@student.hhs.nl>
2020-01-23 12:14:56 +0100

    Bed seizure fixed (Bij veel apparaten door delay werkte dat eerst niet)

--- Raspberry C++/Bed.cpp
+++ Raspberry C++/Bed.cpp
@@ -4,10 +4,8 @@
 Bed::Bed(char* ip) : Device(ip)
 {
 	timeStart = time(nullptr); // timeStart wordt hier op de huidige tijd in secondese sinds 1 januaria 1970 gezet.
-	oneSecond = timeStart + 1; // oneSecond op één seconde later dan timeStart
-	// Door het trucje hierboven kunnen we continu loopen zonder dat we één seconde het programma stil laten staan.
-	// In de loop wordt dus als het ware continu vergeleken of er al één seconde is verstreken.
-	
+	oneSecond = timeStart - 1; // oneSecond op één seconde eerder gezet dan timeStart (zodat hij nog niet optelt)
+
 	//Hieronder worden alle actuatoren en sensoren aangemaakt en op 0 gezet.
 	a1 = new Actuator("1", "0");
 	addActuator("1", a1);
@@ -30,33 +28,44 @@ string Bed::logic(map<string, Device*> dev)
 	string s = ""; // Dit wordt het bericht dat gereturned wordt uit deze functie logic.
 
 	map<string, Actuator*> acts = dev.at("Bed")->getActuators(); // acts bevat alle actuatoren uit "Bed"
+
 	if (seizureLevel == -1){ // initialisatiefase. Zie Bed.h; seizurelevel begint op -1
-		lastAct = stoi(this->s2->getValue()); // De huidige waarde van de druksensor wordt nu opgeslagen in "lastAct" (vage naam)
+		lastAct = stoi(this->s2->getValue()); // De huidige waarde van de druksensor wordt nu opgeslagen in "lastAct" (vage naam, klopt)
 		seizureLevel = 0; // seizurelevel wordt op 0 gezet.
-		s = s + "31;0;"; // Hierin wordt aan het bericht s de waardes 31;0; toegevoerd. 
-		// Het bericht s wordt in de functie gereturned en in control worden de juisteren actuatoren aangestuurd.
-		// In dit geval wordt dus key 31 op waarde 0 gezet.
+		s = s + "31;0;"; // Hierin wordt aan het bericht s de waardes 31;0; toegevoerd.
 	}
 
-	if(oneSecond - time(nullptr) == 0 && seizureLevel < 3) { // Als één seconde verstreken en nog niet hoge seizurelevel...
-		oneSecond = time(nullptr) + 1;
-		seizureTime++;
-		if (lastAct - stoi(this->s2->getValue()) > 200 || lastAct - stoi(this->s2->getValue()) < -200 ) { 
-			// is het verschil tussen de laatste en huidige druksensorwaarde groter dan 200? \
-			// Dan gaat seizurelevel eentje omhoog
-			seizureLevel++;
-		}
+	if (lastAct - stoi(this->s2->getValue()) > 50 || lastAct - stoi(this->s2->getValue()) < -50 ) {
+		// is het verschil tussen de laatste en huidige druksensorwaarde groter dan 50?
+		// Dan gaat seizurelevel eentje omhoog
 		lastAct = stoi(this->s2->getValue());
-
-		if (seizureTime >= 5) { // Na 5 seconde meten, beginnen we opnieuw met meten voor seizures
-			seizureTime = 0;
-		}
+		seizureLevel++;
 	}
 
-	if (seizureLevel >= 2 && seizureTime <= 3) { //Als binnen 3 seconde 2 of meer grote wijzigingen in druk meetbaar zijn, alarm!
-		// cout << endl << endl << endl << "TIMMY KRIJGT EEN EPILEPTISCHE AANVAL!" << endl << endl << endl;
-		s = s + "31;1;"; // notificatie seizure alert op de GUI van de bewaker
-		seizureLevel = -1; // opnieuw seizurelevel initialiseren
+	if(oneSecond - time(nullptr) == 0) { // Als één seconde verstreken...
+		// Door het trucje hierboven kunnen we continu loopen zonder dat we één seconde het programma stil laten staan.
+		// In de loop wordt dus als het ware continu vergeleken of er al één seconde is verstreken.
+		oneSecond = time(nullptr) + 1;
+		seizureTime++; // Er wordt vanaf nu 10 seconde getelt waarin seizures gedetecteerd worden
+	}
+	//cout << "Seizure level: " << seizureLevel << endl;
+	//cout << "Seizure time: " << seizureTime << endl;
+
+	if (seizureTime < 10) { // Na 10 seconde meten, beginnen we opnieuw met meten op seizures
+		if (seizureLevel >= 10){
+			//cout << endl << endl << endl << "TIMMY KRIJGT EEN EPILEPTISCHE AANVAL!" << endl << endl << endl;
+			s = s + "31;1;"; // notificatie seizure alert op de GUI van de bewaker
+			seizureLevel = 0; // seizurelevel resetten
+			seizureTime = 0; // seizuretime resetten
+		}
+		if (seizureLevel == 0)
+			oneSecond = time(nullptr) - 1;
+		if (seizureLevel == 1) // Pas beginnen met seizureTime tellen zodra er 1 seizureLevel is.
+			oneSecond = time(nullptr) + 1;
+	}
+	else if (seizureTime > 10) { // alles resetten na 10 seconde.
+		seizureTime = 0;
+		seizureLevel = 0;
 	}
 
 	if ((time(nullptr) - timeStart) > 16 && goSleep == 0 && alarm == 0) { // Om "11 uur" (na 16 seconde)
@@ -89,9 +98,7 @@ string Bed::logic(map<string, Device*> dev)
 	if (stoi(this->s2->getValue()) < 200 && acts.at("1")->getValue() != "0") {
 		s = s + "1;0;";
 	}
-	*/
+	 */
 
 	return s; // alle actuatoren key en waardes returnen zodat ze in control kunnen worden verwerkt
 }
-
-
