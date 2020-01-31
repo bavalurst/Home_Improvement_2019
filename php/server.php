8e0a6587d50ae976872b3ace9922433e6241cf5e
Sanderb11 <11984491+Sanderb11@users.noreply.github.com>
2020-01-31 16:06:31 +0100

    Kleine aanpassingen website

--- php/server.php
+++ php/server.php
@@ -2,6 +2,7 @@
 
  $host = "127.0.0.1"; //host en port defineren.
  $port = "20205";
+ $port2 = "20206";
  set_time_limit(0);
 
 
@@ -9,6 +10,7 @@
  $result = socket_bind($sock, $host, $port) or die("Could not bind to socket\n");
 
  $result = socket_listen($sock, 3) or die("Could not setup socket listener\n"); //luisteren naar de socket voor inkomende berichten van client.php
+ 
  class Chat {
   function readline() {
    return rtrim(fgets(STDIN));
@@ -20,11 +22,12 @@ do { //binnengekomen bericht opvangen en interpreteren.
   $msg = socket_read($accept, 1024) or die("Could not read input\n");
 
 if($msg == "update") { //Na het verversen van de pagina stuurt client.php een "update" request om de laatste waarde uit van de actuatoren en sensoren op te vragen.
-	//echo "Update requested\n";
+	echo "Update requested\n";
 	
-	$jsonString1 = file_get_contents('data/actuator.json');  // actuatoren ophalen.
-	$jsonString2 = file_get_contents('data/sensor.json'); //sensoren ophalen.
-	$data2 = json_decode($jsonString2, true); //json data omzetten naar array.
+	$jsonString1 = file_get_contents('/home/pi/actuator.json');  // actuatoren ophalen.
+	$jsonString2 = file_get_contents('/home/pi/sensor.json'); //sensoren ophalen.
+	$data1 = json_decode($jsonString1, true); //actuator json data omzetten naar array.
+	$data2 = json_decode($jsonString2, true); //sensor json data omzetten naar array.
 	
 	foreach($data1 as $key => $value) { //waarden van de ene array bij de andere voegen
 		//echo "key: $key \n";
@@ -41,7 +44,7 @@ if($msg == "update") { //Na het verversen van de pagina stuurt client.php een "u
 }
 
 else{ //de server.php mag alleen naar actuatoren schrijven, sensoren kan hij enkel ophalen en versturen naar de client.php om gebruikt te worden in het dashboard.
-	$jsonString = file_get_contents('data/actuator.json');  // actuatoren openen
+	$jsonString = file_get_contents('/home/pi/actuator.json');  // actuatoren openen
 	$data = json_decode($jsonString, true);
 	$msg2 = explode(',', $msg);                       // bericht ui client splitten op kommas, 		bijvoorbeeld "15,1".
 
@@ -51,13 +54,13 @@ else{ //de server.php mag alleen naar actuatoren schrijven, sensoren kan hij enk
 	$data[$ID] = $Value;
 
 	$newJsonString = json_encode($data);
-	file_put_contents('data/actuator.json', $newJsonString); //uitgesplitste waarden naar actuator.json schrijven om geinterpreteerd te kunnen worden door De raspberry Pi.
+	file_put_contents('/home/pi/actuator.json', $newJsonString); //uitgesplitste waarden naar actuator.json schrijven om geinterpreteerd te kunnen worden door De raspberry Pi.
 
 	$msg = trim($msg);
 	echo "Client Says:\t".$msg."\n\n";
  }
 
-  socket_write($accept, $reply, strlen($reply)) or die("Could not write output\n");
+  //socket_write($accept, $reply, strlen($reply)) or die("Could not write output\n");
  }
  while(true);
 
