26cf75b6493a412f4188fa389fd8ce1d3aa771e3
Labrajor <45237702+Labrajor@users.noreply.github.com>
2020-02-04 12:07:47 +0100

    Update Deur.cpp

--- Raspberry C++/Deur.cpp
+++ Raspberry C++/Deur.cpp
@@ -1,6 +1,6 @@
 #include "Deur.h"
 
-Deur::Deur(char* ip int port) : Device(ip, port) {
+Deur::Deur(char* ip) : Device(ip) {
 
 	a1 = new Actuator("22", "0"); //Actuator voor DeurLed binnen
 	addActuator("22", a1);
